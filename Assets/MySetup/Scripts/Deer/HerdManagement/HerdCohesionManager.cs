e0c5d15a835d6d67e9580ccdcb5686ded4d25f64
Dorothea Wunder <dorothea.wunder@student.forsbergsskola.se>
2025-12-15 16:24:39 +0100

    Expanded Need states
    - added notify Herd action
    - 4 steps for state progression instead of 2

--- Assets/MySetup/Scripts/Deer/HerdManagement/HerdCohesionManager.cs
+++ Assets/MySetup/Scripts/Deer/HerdManagement/HerdCohesionManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class HerdCohesionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Herd Center")]
+    public Transform HerdCenter;
+    public float MinToCenter = 5f;
+    public float MaxToCenter = 50f;
+
+    [Header("Individual Cohesion")]
+    public float MinToOtherDeer = 3f;
+    public float MaxToOtherDeer = 10f;
+
+    private HerdManager _herdManager;
+
+    public void InitializeCohesion(HerdManager herdManager)
     {
-        
+        _herdManager = herdManager;
+    }
+    
+    public float DistanceToCenter(Transform deer) =>
+        Vector3.Distance(deer.position, HerdCenter.position);
+
+    public float DistanceToNearestDeer(Transform deer)
+    {
+        float minDistance = float.MaxValue;
+        foreach (var other in _herdManager.DeerList)
+        {
+            if (other.transform == deer) continue;
+            float dist = Vector3.Distance(deer.position, other.transform.position);
+            if (dist < minDistance) minDistance = dist;
+        }
+        return minDistance;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool IsOutsideMaxCenterRange(Transform deer) => DistanceToCenter(deer) > MaxToCenter;
+    public bool IsTooFarFromNearestDeer(Transform deer) => DistanceToNearestDeer(deer) > MaxToOtherDeer;
 }
